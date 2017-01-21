c2ea005b97db8c7688e62dbe3cad3308b551100d
Vlad Pechen <pechenvladislav@gmail.com>
2017-01-21 22:47:27 +0300

    animation fixes

---
+++ Assets/Scripts/AnimationController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationController : MonoBehaviour {
+
+    private Animator animator;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+	void SetAnim(Vector2 dir)
+    {
+        if(dir.x == 1)
+        {
+            animator.SetTrigger("Left");
+        }
+        else if (dir.x == -1)
+        {
+            animator.SetTrigger("Right");
+        }
+        else if (dir.y == 1)
+        {
+            animator.SetTrigger("Front");
+        }
+        else if (dir.y == -1)
+        {
+            animator.SetTrigger("Back");
+        }
+    }
+}
---
+++ Assets/Scripts/Helper.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Helper : MonoBehaviour {
+
+    [SerializeField]
+    private float startDelay;
+
+    private Animator animator;
+
+	// Use this for initialization
+	void Start () {
+
+        animator = GetComponent<Animator>();
+        StartCoroutine(StartHeatyAnimation());
+    }
+	
+	private IEnumerator StartHeatyAnimation()
+    {
+        yield return new WaitForSeconds(startDelay);
+        animator.SetTrigger("Start");
+    }
+}
